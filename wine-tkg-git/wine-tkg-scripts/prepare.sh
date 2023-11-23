e4579458f1c376644bd4ad7040045538e453f604
Mini-Gaunt <jacobh117@gmail.com>
2023-11-23 14:32:21 -0500

    Begin working on fix for osu alt tab on master branch

--- wine-tkg-git/wine-tkg-scripts/prepare.sh
+++ wine-tkg-git/wine-tkg-scripts/prepare.sh
@@ -1080,7 +1080,7 @@ _polish() {
 	# tools/make_makefiles destroys Valve trees - disable on those
 	if [[ "$_custom_wine_source" != *"ValveSoftware"* ]]; then
 	  git add * && true
-#	  tools/make_makefiles
+	  tools/make_makefiles
 	fi
 
 	echo -e "\nRunning make_vulkan" >> "$_where"/prepare.log && dlls/winevulkan/make_vulkan >> "$_where"/prepare.log 2>&1
